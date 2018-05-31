c8e3ceb20d83c9542a4b91b8edc71e30a0cb1e0f
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 18:39:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length > 1
+  last_word = "and " 
+  last_word << (array[-1])
+end
 
+if array.length == 1
+  array.join
+elsif array.length < 3
+  array.pop
+  array.push(last_word)
+  array.join(" ")
+else
+  array.pop
+  array.push(last_word)
+  array.join(", ")
+end
 end
