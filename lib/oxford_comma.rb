8def454eba4902303d64cbcb76841d50583e9398
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 18:36:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,14 @@ def oxford_comma(array)
   last_word = "and " 
   last_word << (array[-1])
 end
-  
+
+if array.length < 3
+  array.pop
+  array.push(last_word)
+  array.join(" ")
+else
   array.pop
   array.push(last_word)
   array.join(", ")
+end
 end
